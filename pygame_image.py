6f2b89c25ee8c40cbf458cb5868eaf73ecadb0e4
kosugi <C0A2216043@edu.teu.ac.jp>
2023-06-13 17:31:11 +0900

    練習６

--- pygame_image.py
+++ pygame_image.py
@@ -18,9 +18,9 @@ def main():
             if event.type == pg.QUIT: return
         screen.blit(bg_img, [-tmr, 0])
         screen.blit(bg_img, [1600-tmr, 0])
-        screen.blit(kk_imgs[tmr%2],[300,200])
+        screen.blit(kk_imgs[tmr%100//50],[300,200])
         pg.display.update()
-        tmr += 1        
+        tmr += 1
         clock.tick(100)
 
 
